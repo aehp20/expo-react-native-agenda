3002c0bb100faaec808d29c68131b9ef3fadce91
Aron HERRERA PONTE <aronherrera2009@gmail.com>
2024-06-25 17:29:29 +0200

    complete Box storybook

--- libs/design-system/atoms/box/Box.stories.tsx
+++ libs/design-system/atoms/box/Box.stories.tsx
@@ -1,28 +1,18 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { View } from "react-native";
+import { Text } from "react-native";
 
 import Box from "./Box";
 
 const meta: Meta<typeof Box> = {
   title: "Box",
   component: Box,
-  argTypes: {},
-  args: {},
-  decorators: [
-    (Story) => (
-      <View style={{ alignItems: "center", justifyContent: "center", flex: 1 }}>
-        <Story />
-      </View>
-    ),
-  ],
 };
 
 export default meta;
 
-export const Basic: StoryObj<typeof Box> = {};
-
-// export const AnotherExample: StoryObj<typeof Box> = {
-//   args: {
-//     text: "Another example",
-//   },
-// };
+export const Basic: StoryObj<typeof Box> = {
+  args: {
+    style: { padding: 4, backgroundColor: "yellow" },
+    children: <Text>Hello</Text>,
+  },
+};
